ae9fd88b2fba6ad01d5a3e17ce15154fe84e3f84
Olatunji <abdulafeezoluwashola2@gmail.com>
2025-01-12 10:27:29 +0100

    await next

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 // const url = prompt('Enter your Url: ');
-const div = document.getElementById('para')
-const btnid = document.getElementById('btn')
+const div = document.getElementById("para");
+const btnid = document.getElementById("btn");
 async function getData() {
   const response = await fetch(
     "https://official-joke-api.appspot.com/random_joke"
   );
   const result = await response.json();
-  div.innerHTML = result.value
-  document.getElementById('para').innerHTML = `${result.setup}- ${result.punchline}`;
+  div.innerHTML = result.value;
+  document.getElementById(
+    "para"
+  ).innerHTML = `${result.setup}- ${result.punchline}`;
   return result;
 }
-
 
 (async () => {
   const result = await getData();
 })();
-btnid.addEventListener('click', getData)
+btnid.addEventListener("click", getData);
