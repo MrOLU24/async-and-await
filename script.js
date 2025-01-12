5130fcfeea95db5c72d58c58e9506d26a8157b5b
Olatunji <abdulafeezoluwashola2@gmail.com>
2025-01-12 10:15:34 +0100

    learning how api works

---
+++ script.js
@@ -0,0 +1,17 @@
+// const url = prompt('Enter your Url: ');
+const div = document.getElementById('para')
+const btnid = document.getElementById('btn')
+async function getData() {
+  const response = await fetch(
+    "https://official-joke-api.appspot.com/random_joke"
+  );
+  const result = await response.json();
+  div.innerHTML = result.value
+  document.getElementById('para').innerHTML = `${result.setup}- ${result.punchline}`;
+  return result;
+}
+btnid.addEventListener('click', getData)
+getData()
+// (async () => {
+//   const result = await getData();
+// })();
